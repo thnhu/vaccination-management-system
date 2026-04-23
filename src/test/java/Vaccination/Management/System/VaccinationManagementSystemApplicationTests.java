c7abda46a5d69ee17898d90b1fa37525c5932d02
thnhu <hnhu3050@gmail.com>
2026-04-23 22:13:54 +0700

    commit

---
+++ src/test/java/Vaccination/Management/System/VaccinationManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package Vaccination.Management.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaccinationManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
